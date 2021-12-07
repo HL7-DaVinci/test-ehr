00194b33b5a3f2dbf5014155061187940506a6a7
Sahil Malhotra <smalhotra@mitre.org>
2021-12-07 02:15:32 -0500

    remove docker_profile env from production runner

--- dockerRunnerProd.sh
+++ dockerRunnerProd.sh
@@ -3,8 +3,6 @@
 # Handle closing application on signal interrupt (ctrl + c)
 trap "kill $LOAD_DATA_PID $SERVER_PID; gradle --stop; exit" INT
 
-# Set environment variables
-export DOCKER_PROFILE="true"
 mkdir logs 
 # Reset log file content for new application boot
 echo "*** Logs for 'gradle bootRun' ***" > ./logs/runner.log
