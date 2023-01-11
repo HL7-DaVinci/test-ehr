e80acb6abcfa2ed8ddbc8fa2ec0a6e1eeedf4611
daniel.vargas <daniel.vargas@lantanagroup.com>
2023-01-10 22:29:38 -0600

    Adjusted finalRedirectURI variable. The auth_redirect_host will need to be configured for http or https. Additionally, the port number will only get appended if there is no auth_redirect_host variable. Added https to audit_redirect_host docker property

--- src/main/java/org/hl7/davinci/ehrserver/authproxy/AuthProxy.java
+++ src/main/java/org/hl7/davinci/ehrserver/authproxy/AuthProxy.java
@@ -141,7 +141,8 @@ public class AuthProxy {
    */
   private String _parseRedirect(Map<String, String> reqParamValue, HttpServletRequest request) {
     String currentRedirectURI = reqParamValue.get("redirect_uri");
-    String finalRedirectURI = "http://" + ((System.getenv("DOCKER_PROFILE") != null && System.getenv("DOCKER_PROFILE").equals("true")) && Config.get("auth_redirect_host") != null ? Config.get("auth_redirect_host") : request.getLocalName()) + ":" + request.getLocalPort() + "/test-ehr/_auth/" + reqParamValue.get("launch") + "?redirect_uri=" + currentRedirectURI;
+    //String finalRedirectURI = "http://" + ((System.getenv("DOCKER_PROFILE") != null && System.getenv("DOCKER_PROFILE").equals("true")) && Config.get("auth_redirect_host") != null ? Config.get("auth_redirect_host") : request.getLocalName()) + ":" + request.getLocalPort() + "/test-ehr/_auth/" + reqParamValue.get("launch") + "?redirect_uri=" + currentRedirectURI;
+    String finalRedirectURI = (Config.get("auth_redirect_host") != null ? Config.get("auth_redirect_host") : request.getLocalName() + ":" + request.getLocalPort()) + "/test-ehr/_auth/" + reqParamValue.get("launch") + "?redirect_uri=" + currentRedirectURI;
     reqParamValue.put("redirect_uri", finalRedirectURI);
     payloadDAO.updateRedirect(reqParamValue.get("launch"), finalRedirectURI);
     return paramFormatter(reqParamValue);
